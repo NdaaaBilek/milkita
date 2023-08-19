9ce3c4886e5f9d87468d0d5617b8c0596547bdcf
Lu asik bang <98279739+NdaaaBilek@users.noreply.github.com>
2023-08-19 22:10:59 +0700

    Update search-lirik.cjs

--- plugins/search-lirik.cjs
+++ plugins/search-lirik.cjs
@@ -1,9 +1,10 @@
 var { lyrics } = require("api-dylux")
 var handler = async (m, {conn, text}) => {
-var res = await lyrics("style")
+  if (!text) throw `Contoh .lyrics style`
+var res = await lyrics(text)
 conn.sendFile(m.chat, res.thumb, "hhe.jpg", res.lyrics, m)
 }
-handler.help = handler.command = ['lyric']
+handler.help = handler.command = ['lyrics']
 handler.tags = ['search']
 
 module.exports = handler
