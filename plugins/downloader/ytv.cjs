d5807747ef03091a2f86f2b383c94bd0aa384953
Lu asik bang <98279739+NdaaaBilek@users.noreply.github.com>
2023-08-20 23:44:03 +0700

    Update ytv.cjs

--- plugins/downloader/ytv.cjs
+++ plugins/downloader/ytv.cjs
@@ -2,8 +2,8 @@ var { ytmp3 } = require("api-dylux")
 var handler = async (m, {conn, text}) => {
   if (!text) throw `Masukkan Link Yt!`
   let res = await ytmp3(text)
-  
-  conn.sendMessage(m.chat, { document: { url: res.dl_url}, mimetype: 'audio/mpeg', fileName: `sukses.mp3`}, {quoted: m})
+  conn.sendFile(m.chat, res.dl_url, null, null, m)
+  //conn.sendMessage(m.chat, { document: { url: res.dl_url}, mimetype: 'audio/mpeg', fileName: `sukses.mp3`}, {quoted: m})
 }
 handler.help = handler.command = ['ytmp3', 'ytv']
 handler.tags = ['downloader']
