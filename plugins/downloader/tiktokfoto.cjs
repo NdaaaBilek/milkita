ce81270cbf7ef442e33c84f79409dc61d1ed2602
Lu asik bang <98279739+NdaaaBilek@users.noreply.github.com>
2023-08-27 18:07:58 +0700

    Update tiktokfoto.cjs

--- plugins/downloader/tiktokfoto.cjs
+++ plugins/downloader/tiktokfoto.cjs
@@ -1,7 +1,7 @@
 var handler = async (m, {conn, text }) => {
   if (!text) throw `Linknya mana?`
   var res = await fetch(`https://api.tiklydown.eu.org/api/download?url=${text}`)
-  var json = await fetch.json()
+  var json = await res.json()
   for (let i of json.images) {
     conn.sendFile(m.chat, i.url, null, null, m)
   }
