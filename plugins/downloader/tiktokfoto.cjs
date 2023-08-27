404ab1a02acd64b4c20c8e878b317bc588b62470
Lu asik bang <98279739+NdaaaBilek@users.noreply.github.com>
2023-08-27 18:06:31 +0700

    Create tiktokfoto.cjs

---
+++ plugins/downloader/tiktokfoto.cjs
@@ -0,0 +1,12 @@
+var handler = async (m, {conn, text }) => {
+  if (!text) throw `Linknya mana?`
+  var res = await fetch(`https://api.tiklydown.eu.org/api/download?url=${text}`)
+  var json = await fetch.json()
+  for (let i of json.images) {
+    conn.sendFile(m.chat, i.url, null, null, m)
+  }
+}
+handler.help = handler.command = ["tiktokfoto", "ttslide"]
+handler.tags = ['downloader']
+
+module.exports = handler
